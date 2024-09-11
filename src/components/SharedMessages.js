bb2f04dd743ef1765f844795be3d2483c21ae1c2
longnguyencoder <long0398481719@gmail.com>
2024-09-11 23:33:11 +0700

    update : fix image media, setting

--- src/components/SharedMessages.js
+++ src/components/SharedMessages.js
@@ -1,97 +1,110 @@
 import { Box, Grid, IconButton, Stack, Typography } from "@mui/material";
 import React from "react";
-import { useTheme } from '@mui/material/styles';
-import { useDispatch } from 'react-redux';
+import { useTheme } from "@mui/material/styles";
+import { useDispatch } from "react-redux";
 import { toggleSidebar, updateSidebarType } from "../redux/slices/app";
 import { Backspace, CaretLeft } from "phosphor-react";
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 import { faker } from "@faker-js/faker";
 import { SHARED_DOCS, SHARED_LINKS } from "../data";
 import { DocMsg, LinkMsg } from "./Conversation/MsgTypes";
 
-
-
-
 const SharredMessages = () => {
   const theme = useTheme();
-  const dispatch =useDispatch();
+  const dispatch = useDispatch();
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-    return (
-       <Box sx={{width:320, height: "100vh"}}>
-            <Stack sx={{height:"100%"}}>
-                 {/* header */}
-            <Box sx={{
-              boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
-              width:"100%",
-              backgroundColor: theme.palette.mode === "Light" ? "#F8FAFF" : theme.palette.background,
-            }}>
-          <Stack sx={{height:"100%",p:2}} direction="row" alignItems={"center"} justifyContent={'space-between'} spacing={3}>
-          <IconButton onClick={()=>{    
-            dispatch(updateSidebarType("CONTACT"));
-          }}>
-            <CaretLeft/>
-          </IconButton>
-          <Typography variant="subtitle2">Share Messages</Typography>
-       
+  return (
+    <Box sx={{ width: 320, height: "100vh" }}>
+      <Stack sx={{ height: "100%" }}>
+        {/* header */}
+        <Box
+          sx={{
+            boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
+            width: "100%",
+            backgroundColor:
+              theme.palette.mode === "Light"
+                ? "#F8FAFF"
+                : theme.palette.background,
+          }}
+        >
+          <Stack
+            sx={{ height: "100%", p: 2 }}
+            direction="row"
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            spacing={3}
+          >
+            <IconButton
+              onClick={() => {
+                dispatch(updateSidebarType("CONTACT"));
+              }}
+            >
+              <CaretLeft />
+            </IconButton>
+            <Typography variant="subtitle2">Share Messages</Typography>
           </Stack>
-            </Box>
+        </Box>
 
-            <Tabs sx={{px:2, pt:2}} value={value} onChange={handleChange} centered>
-        <Tab label="Media" />
-        <Tab label="Links" />
-        <Tab label="Docs" />
-      </Tabs>
+        <Tabs
+          sx={{ px: 2, pt: 2 }}
+          value={value}
+          onChange={handleChange}
+          centered
+        >
+          <Tab label="Media" />
+          <Tab label="Links" />
+          <Tab label="Docs" />
+        </Tabs>
 
-              {/* body */}
-                <Stack sx= {{
-                height:"100%",
-                position: "relative", 
-                flexGrow:1, 
-                overflow: "scroll"}} 
-                p= {3} spacing={value ===1 ? 1: 3} >
+        {/* body */}
+        <Stack
+          sx={{
+            height: "100%",
+            position: "relative",
+            flexGrow: 1,
+            overflow: "scroll",
+          }}
+          p={3}
+          spacing={value === 1 ? 1 : 3}
+        >
+          {/* <Conversation starred={true} /> */}
+          {(() => {
+            switch (value) {
+              case 0:
+                return (
+                  <Grid container spacing={2}>
+                    {[0, 1, 2, 3, 4, 5, 6].map((el) => (
+                      <Grid item xs={4}>
+                        <img
+                          src={faker.image.city()}
+                          alt={faker.internet.userName()}
+                        />
+                      </Grid>
+                    ))}
+                  </Grid>
+                );
+              case 1:
+                // Links
+                return SHARED_LINKS.map((el) => <LinkMsg el={el} />);
 
+              case 2:
+                // Docs
+                return SHARED_DOCS.map((el) => <DocMsg el={el} />);
 
-                  {(()=>{
-                    switch (value) {
-                      case 0:
-                        //Images
-                        return (
-                        <Grid container spacing={2}>
-                            {
-                              [0, 1, 2, 3, 4, 5, 6,7,8,9].map((el) => {
-                               return <Grid item xs={4}>
-                                  <img 
-                                  src={faker.image.avatar()} 
-                                  alt={faker.name.fullName()}
-                                  />
-                                </Grid>;
-                              })
-                            }
-                        </Grid>
-                        );
-                        case 1:
-                        // Links
-                        return SHARED_LINKS.map((el) => <LinkMsg el={el} />)
-                        break;
-                        case 2:
-                        // Docs
-                        return SHARED_DOCS.map((el) => <DocMsg el={el} />)
-                        
-                    
-                      default:
-                        break;
-                    }
-                  })()}
-              </Stack>
-            </Stack>
-       </Box>
-    )
-}
+              default:
+                break;
+            }
+          })()}
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
 
 export default SharredMessages;
